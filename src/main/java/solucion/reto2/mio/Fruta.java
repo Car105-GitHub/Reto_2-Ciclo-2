a202f9fe62d4f8bbe06f890a2a5472cf6d541b11
Quon <quon@MacBook-Pro-de-Quon-5.local>
2021-08-06 22:19:45 -0500

    commit furtas

--- src/main/java/solucion/reto2/mio/Fruta.java
+++ src/main/java/solucion/reto2/mio/Fruta.java
@@ -2,13 +2,49 @@ package solucion.reto2.mio;
 
 public class Fruta {
     //Atributos
+
+    private static final int GRAMAJE_BASE_VENTA = 200;
+    private String nombre;
+    private double precioBase;
+    protected int gramosVenta;
+    protected boolean importada = false;
+
+
     //Constructores
     public Fruta(String pNombre, double pPrecio){
+        this.nombre = pNombre;
+        this.precioBase = pPrecio;
+        this.gramosVenta = Fruta.GRAMAJE_BASE_VENTA;   
     }
-    public Fruta(String pNombre, double pPrecio, int }
-    public Fruta(String pNombre, double pPrecio, int pImportada){
+    public Fruta(String pNombre, double pPrecio, int pGramosVenta){
+
+        this(pNombre, pPrecio);
+        
+        if(pGramosVenta < Fruta.GRAMAJE_BASE_VENTA){
+            this.gramosVenta = Fruta.GRAMAJE_BASE_VENTA;
+        }else{
+            this.gramosVenta = pGramosVenta;
+        }
     }
+
+    public Fruta(String pNombre, double pPrecio, int pGramosVenta, boolean pImportada){
+        this(pNombre, pPrecio, pGramosVenta);
+        this.importada = pImportada;
+    }
+
+
+
     //Métodos
     public double calcularPrecio(){
+        double precioVenta =0;
+        
+        if(this.importada){
+            precioVenta = (this.precioBase * this.gramosVenta) * 1.10;
+        }else{
+            precioVenta = (this.precioBase * this.gramosVenta);
+        }
+        return precioVenta;
+
     }
+    
     }
