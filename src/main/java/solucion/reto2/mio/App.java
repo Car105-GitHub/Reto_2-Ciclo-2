953be85bd728a7f6cdcddb7c91cc29556640668e
Quon <quon@MacBook-Pro-de-Quon-5.local>
2021-08-06 20:58:23 -0500

    commit inicial 1

---
+++ src/main/java/solucion/reto2/mio/App.java
@@ -0,0 +1,13 @@
+package solucion.reto2.mio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
